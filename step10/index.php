39157595554633a42345fbf5671113a6b2717c60
busybee333 <43186978+busybee333@users.noreply.github.com>
2018-09-15 16:36:46 -0400

    Add files via upload

---
+++ step10/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Romi
+ * Date: 6/23/2018
+ * Time: 6:13 PM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Step 10</title>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="site.con.js"></script>
+    <!--<script src="site.min.js"></script>-->
+</head>
+<body>
+</body>
+</html>
